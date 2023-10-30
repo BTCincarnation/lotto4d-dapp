ae9698c6f8f19a3da326f5332f77b9734e82fa05
lotto4d <142324633+BTCincarnation@users.noreply.github.com>
2023-10-30 21:57:25 +0700

    Update mainnet

--- src/lottery.js
+++ src/lottery.js
@@ -2,7 +2,7 @@ import { abi } from "./abi";
 import Web3 from "web3";
 
 const address = "0x07E78d26FCfF2E3bcAb75AB2cCaA7AFD5E84cEe2";
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc-mumbai.maticvigil.com'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://polygon-mainnet.infura.io));
 const lotto4DStats = new web3.eth.Contract(abi, address);
 export default lotto4DStats;
 
